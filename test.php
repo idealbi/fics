1ad67f336dda9f5e157d3f329ccd0728d575a75b
idealBI <125181140+idealbi@users.noreply.github.com>
2023-03-02 09:53:47 +0200

    Update test.php

--- test.php
+++ test.php
@@ -4,16 +4,19 @@
 $connectionInfo = array("UID" => "web_app_user", "pwd" => "P@ss1234", "Database" => "fics_db", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:srv-db-idealbi.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
-if( $conn ) {
-     echo "Connection established.<br />";
-   }else{
+if( $conn ) 
+{
+     
+     echo "Connection established....quering db now<br />";
+     $sql = "SELECT  [TestColumn]FROM [dbo].[Test1]";
+     $result = sqlsrv_query($conn, $sql);
+     while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
+              echo "Test Table : ".$row."<br>" ;
+          }
+   }
+   else{
      echo "Connection could not be established.<br />";
      die( print_r( sqlsrv_errors(), true));
    }
-$sql = "SELECT  [TestColumn]FROM [dbo].[Test1]";
-$result = sqlsrv_query($conn, $sql);
 
-while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
-    echo "Test Table : '.$row ;
-}
 ?>
