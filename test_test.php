3d2937ec78e6293caa852503a113010965763642
idealBI <125181140+idealbi@users.noreply.github.com>
2023-03-05 13:48:54 +0200

    Update test_test.php

--- test_test.php
+++ test_test.php
@@ -161,7 +161,22 @@ echo " Tests  results3 : ".$array_length."<br>";
 ?>
 
 
-  
+<h1>Cascading Dropdown Example</h1>
+
+<form name="form1" id="form1" action="/action_page.php">
+<select id="dropdown1" onchange="getDivision()">>
+  <option value="1">Option 1</option>
+  <option value="2">Option 2</option>
+  <option value="3">Option 3</option>
+</select>
+
+<select id="dropdown2">
+  <option value="a">Option a</option>
+  <option value="b">Option b</option>
+  <option value="c">Option c</option>
+</select>
+
+</form>  
  
 
   </div>
