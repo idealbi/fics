f5097e2852c5844e5d215594076d2638edac0c5c
idealBI <125181140+idealbi@users.noreply.github.com>
2023-03-06 14:33:15 +0200

    Update data_admin.php

--- data_admin.php
+++ data_admin.php
@@ -1,4 +1,9 @@
  <p>Name: <?php echo $_POST["companyid"]." ".$_POST["lname"]; ?></p>
     <p>E-mail: <?php echo $_POST["formCompnay"]; ?>
-	<br>Cell Number : <?php echo $_POST["formDivision"]; ?></p>
-  
+	<br>Cell Number : <?php 
+	    echo $_POST["formDivision"]; ?></p>
+  <?php 
+$div= $_POST["formDivision"];
+$Ismulty = substr_count($div, ',');
+echo "Number : ".$Ismulty;
+?>
