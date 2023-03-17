eeb709efafc2c02d56b59a9263b8f08fa9471ba4
idealBI <125181140+idealbi@users.noreply.github.com>
2023-03-17 10:59:23 +0200

    Update resultscon.php

--- resultscon.php
+++ resultscon.php
@@ -145,7 +145,17 @@ img_logo {
      
      
    <?php
-include(dbcon.php)
+$serverName = "mssql-115975-0.cloudclusters.net,19881";
+$connectionInfo = array( "Database"=>"idealbi_db", "UID"=>"web_app_user", "PWD"=>"P@ss1234");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+echo "Test new 1";
+if( $conn ) {
+     echo "Connection established.<br />";
+}else{
+     echo "Connection could not be established.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+
  
 if ($conn)
 {
