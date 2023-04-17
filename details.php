38b368d5fe7384adf67d390b55286c08c9a3274a
idealBI <125181140+idealbi@users.noreply.github.com>
2023-04-18 00:05:38 +0200

    Update details.php

--- details.php
+++ details.php
@@ -333,15 +333,12 @@ $result = sqlsrv_query($conn, $sql_p);
 </font></div>
     <div class="col-9">
       
-	  
-		<div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">	
-			<div><a href="<?php echo $Behaviouralpdffile;?>" class="btn btn-info btn-sm" role="button btn-sm" download="<?php echo $BehaviouralDownFileName;?>">Download : <?php echo $Behavioural ?></a></div>
- 
-<hr>
-<img src=" <?php echo $Behaviouralfile;?>" alt="" width="100%" height="400">
+	  <br>
+		<a href="<?php echo $Behaviouralpdffile;?>" class="btn btn-info btn-sm" role="button btn-sm" download="<?php echo $BehaviouralDownFileName;?>">Download : <?php echo $Behavioural ?></a>
 <br>
-<a href="<?php echo $Behaviouralpdffile;?>" class="btn btn-info btn-sm" role="button btn-sm" download="<?php echo $BehaviouralDownFileName;?>">Download : <?php echo $Behavioural ?></a>
-</div>
+<img src=" <?php echo $Behaviouralfile;?>" alt="" width="80%" height="400">
+<br>
+
 
 
 <hr>
@@ -352,7 +349,7 @@ $result = sqlsrv_query($conn, $sql_p);
 
 <a href="<?php echo $Languagepdffile;?>" class="btn btn-info btn-sm" role="button btn-sm "download="<?php echo $LanguageDownFileName;?>" >Download: <?php echo $Language;?></a>
 <br>
-<img src="<?php echo $Languagefile;?>" alt="" width="100%" height="400">
+<img src="<?php echo $Languagefile;?>" alt="" width="80%" height="400">
 
 
   </div>
