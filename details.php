e4e6f4e93867c440941e0b081a6e70e1e4839da6
idealBI <125181140+idealbi@users.noreply.github.com>
2023-04-17 23:36:24 +0200

    Update details.php

--- details.php
+++ details.php
@@ -336,8 +336,10 @@ $result = sqlsrv_query($conn, $sql_p);
 		<div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">	
 			<div><a href="<?php echo $Behaviouralpdffile;?>" class="btn btn-info btn-sm" role="button btn-sm" download="<?php echo $BehaviouralDownFileName;?>">Download : <?php echo $Behavioural ?></a></div>
  
-
+<hr>
 <img src=" <?php echo $Behaviouralfile;?>" alt="" width="100%" height="400">
+<br>
+<a href="<?php echo $Behaviouralpdffile;?>" class="btn btn-info btn-sm" role="button btn-sm" download="<?php echo $BehaviouralDownFileName;?>">Download : <?php echo $Behavioural ?></a>
 </div>
 </div>
 <hr>
